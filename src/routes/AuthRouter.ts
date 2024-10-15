60948931a1a6712b2a1de7beb58369c427bd1e13
Pratik <pratikkhatri520@gmail.com>
2024-10-15 12:47:21 +0530

    temp changes

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -1,15 +1,29 @@
 import express from "express";
 
 const router = express.Router();
-import { registerC, loginC, editUser, fetchUsers , getUser, deleteUser} from "../controllers/AuthController.js";
+import {
+  registerC,
+  loginC,
+  editUser,
+  fetchUsers,
+  getUser,
+  deleteUser,
+} from "../controllers/AuthController.js";
 import { CheckPrimeOptions } from "crypto";
 import { checkPermissions } from "../middleware/checkPermissions.js";
 import authenticate from "../middleware/authentication.js";
-router.post("/add-user", authenticate,checkPermissions("add-user"), registerC);
+router.post("/add-user", authenticate, registerC);
+// router.post("/add-user", authenticate,checkPermissions("add-user"), registerC);
 router.get("/fetch-users", authenticate, fetchUsers);
 router.get("/get-user", authenticate, getUser);
-router.put("/edit-user/:id", authenticate,checkPermissions("edit-user"), editUser);
-router.delete("/delete-user", authenticate,checkPermissions("delete-user"), deleteUser);
+router.put("/edit-user/:id", authenticate, editUser);
+// router.put("/edit-user/:id", authenticate,checkPermissions("edit-user"), editUser);
+router.delete(
+  "/delete-user",
+  authenticate,
+  checkPermissions("delete-user"),
+  deleteUser
+);
 // router.post("/add-user", registerC);
 
 router.post("/login", loginC);
