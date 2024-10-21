b73cb051b81290ec2865ff92ff17c6b1471070aa
Pratik <pratikkhatri520@gmail.com>
2024-10-22 00:33:39 +0530

    added get production by id route

--- src/routes/ProductionRouter.ts
+++ src/routes/ProductionRouter.ts
@@ -5,12 +5,14 @@ import {
   updateProductionById,
   getUserAssignedProduction,
   generatePdf,
+  fetchProductionById,
 } from "../controllers/ProductionController.js";
 
 const router = express.Router();
 router
   .get("/get-all-productions", fetchProductions)
   .get("/get-my-productions", getUserAssignedProduction)
+  .get("/getProductionById/:id", fetchProductionById)
   .get("/generate-pdf/:id", generatePdf)
   .post("/start-new", startNewProduction)
   .put("/update/:id", updateProductionById);
