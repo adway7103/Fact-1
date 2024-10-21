25914c99e0f0273ed9db3d47ee276d0dd6c416ec
Pratik <pratikkhatri520@gmail.com>
2024-10-22 00:32:21 +0530

    added lifecycle routes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import TodoRouter from "./routes/TodoRouter.js";
 import authenticate from "./middleware/authentication.js";
 import inventoryRouter from "./routes/Inventory.js";
 import productionRouter from "./routes/ProductionRouter.js";
+import lifecycleRouter from "./routes/LifecycleRouter.js";
 import cors from "cors";
 import AddPermissions from "./controllers/Rules.js";
 // import roleRouter from "./routes/RoleRouter.js";
@@ -20,6 +21,7 @@ app.use("/auth", authRouter);
 app.use("/todo", authenticate, TodoRouter);
 app.use("/inventory", authenticate, inventoryRouter);
 app.use("/production", authenticate, productionRouter);
+app.use("/lifecycle", authenticate, lifecycleRouter);
 // app.use("/role",authenticate,checkPermissions("add-role"), roleRouter);
 app.post("/role", AddPermissions);
 
