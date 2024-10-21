834e1908ac24742ae2a049b59b115c8f07feffcc
Pratik <pratikkhatri520@gmail.com>
2024-10-22 00:31:14 +0530

    added lifecycle routes

--- dist/index.js
+++ dist/index.js
@@ -15,6 +15,7 @@ import TodoRouter from "./routes/TodoRouter.js";
 import authenticate from "./middleware/authentication.js";
 import inventoryRouter from "./routes/Inventory.js";
 import productionRouter from "./routes/ProductionRouter.js";
+import lifecycleRouter from "./routes/LifecycleRouter.js";
 import cors from "cors";
 import AddPermissions from "./controllers/Rules.js";
 const app = express();
@@ -24,6 +25,7 @@ app.use("/auth", authRouter);
 app.use("/todo", authenticate, TodoRouter);
 app.use("/inventory", authenticate, inventoryRouter);
 app.use("/production", authenticate, productionRouter);
+app.use("/lifecycle", authenticate, lifecycleRouter);
 // app.use("/role",authenticate,checkPermissions("add-role"), roleRouter);
 app.post("/role", AddPermissions);
 app.get("/", (req, res) => {
