7a96858a39060197a75970f08c18541af5ae2904
Pratik <pratikkhatri520@gmail.com>
2024-10-22 21:51:36 +0530

    fix

--- src/models/Lifecycle.ts
+++ src/models/Lifecycle.ts
@@ -15,8 +15,9 @@ interface ILifecycle extends mongoose.Document {
   rolls: LifeCycleDetails[];
   markAsDone: boolean;
   lotNo: string;
-  currentStage: StageEnum;
+//   currentStage: StageEnum;
   stages: StageDetails[];
+  completionDate?: Date; // Add this line
 }
 
 // Create the ILifecycle schema
@@ -34,16 +35,19 @@ const LifecycleSchema = new mongoose.Schema<ILifecycle>(
       type: Boolean,
       default: false,
     },
-    currentStage: {
-      type: String,
-      enum: Object.values(StageEnum),
-      required: true,
-      default: StageEnum.Karigar,
-    },
+    // currentStage: {
+    //   type: String,
+    //   enum: Object.values(StageEnum),
+    //   required: true,
+    //   default: StageEnum.Karigar,
+    // },
     stages: {
       type: [StageDetailsSchema],
       required: true,
-    },
+    }, completionDate: {
+        type: Date,
+        required: false,
+      },
   },
   {
     timestamps: true,
