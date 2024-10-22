716d6eff6502b1be1bee22c42b41fe444799f8a3
Pratik <pratikkhatri520@gmail.com>
2024-10-22 21:52:56 +0530

    added lifecycle routers

--- src/routes/LifecycleRouter.ts
+++ src/routes/LifecycleRouter.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import {
   startNewLifecycle,
-  fetchProductions,
+  fetchLifecycles,
+  updateLifecycle,
+  fetchLifecycleById,
+  startLifecycleNewStage,
 } from "../controllers/LifecycleController.js";
 const router = express.Router();
 
 router
   .post("/start-new-lifecycle", startNewLifecycle)
-  .get("/get-all-lifecycle", fetchProductions);
+  .post("/:id/new-stage", startLifecycleNewStage)
+  .get("/get-all-lifecycle", fetchLifecycles)
+  .get("/getLifecycleById/:id", fetchLifecycleById)
+  .put("/update/:id/:stageId", updateLifecycle);
 
 export default router;
