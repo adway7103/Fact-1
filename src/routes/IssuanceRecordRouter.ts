136c8179beef77a197e1226c5df92649af7da441
Pratik <pratikkhatri520@gmail.com>
2024-10-25 23:35:31 +0530

    added issuance record router

---
+++ src/routes/IssuanceRecordRouter.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+import {
+  issueInventoryItems,
+  fetchIssuanceRecords,
+} from "../controllers/IssuanceRecordController.js";
+const router = express.Router();
+
+router
+  .post("/issue-inventory-items", issueInventoryItems)
+  .get("/issuance-records", fetchIssuanceRecords);
+
+export default router;
