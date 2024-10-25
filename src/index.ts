79e2084aec42f6b45e634cc5a31a0fd57417d40a
Pratik <pratikkhatri520@gmail.com>
2024-10-25 23:35:58 +0530

    added issuance router

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import authenticate from "./middleware/authentication.js";
 import inventoryRouter from "./routes/Inventory.js";
 import productionRouter from "./routes/ProductionRouter.js";
 import lifecycleRouter from "./routes/LifecycleRouter.js";
+import issuanceRouter from "./routes/IssuanceRecordRouter.js"
 import cors from "cors";
 import AddPermissions from "./controllers/Rules.js";
 // import roleRouter from "./routes/RoleRouter.js";
@@ -22,6 +23,7 @@ app.use("/todo", authenticate, TodoRouter);
 app.use("/inventory", authenticate, inventoryRouter);
 app.use("/production", authenticate, productionRouter);
 app.use("/lifecycle", authenticate, lifecycleRouter);
+app.use("/issuance", authenticate, issuanceRouter);
 // app.use("/role",authenticate,checkPermissions("add-role"), roleRouter);
 app.post("/role", AddPermissions);
 
