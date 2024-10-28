80e9593a06c9d3da0f53b9a2484899e456054737
Pratik <pratikkhatri520@gmail.com>
2024-10-28 22:43:33 +0530

    added router

--- src/routes/IssuanceRecordRouter.ts
+++ src/routes/IssuanceRecordRouter.ts
@@ -4,12 +4,14 @@ import {
   issueInventoryItems,
   fetchIssuanceRecords,
   fetchIssuanceRecordsWitId,
+  fetchIssuanceRecordsWithUserId,
 } from "../controllers/IssuanceRecordController.js";
 const router = express.Router();
 
 router
   .post("/issue-inventory-items", issueInventoryItems)
   .get("/issuance-records", fetchIssuanceRecords)
-  .get("/issuance-record/:id", fetchIssuanceRecordsWitId);
+  .get("/issuance-record/:id", fetchIssuanceRecordsWitId)
+  .get("/my-issuance-record", fetchIssuanceRecordsWithUserId);
 
 export default router;
