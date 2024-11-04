3eac2dde2538570f0d9914fe9ee48bbb55cfaf27
Pratik <pratikkhatri520@gmail.com>
2024-11-04 11:35:15 +0530

    fix

--- src/models/Lifecycle.ts
+++ src/models/Lifecycle.ts
@@ -20,6 +20,14 @@ interface ILifecycle extends mongoose.Document {
   type: TypeEnum;
   //   currentStage: StageEnum;
   stages: StageDetails[];
+  brand: string;
+  accessories: string;
+  mainThread: string;
+  contrastThread: string;
+  insideThread: string;
+  washCard: string;
+  embroidery: string;
+  zip: string;
 }
 
 // Create the ILifecycle schema
@@ -39,7 +47,38 @@ const LifecycleSchema = new mongoose.Schema<ILifecycle>(
     },
     type: {
       type: String,
-
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    accessories: {
+      type: String,
+      required: true,
+    },
+    mainThread: {
+      type: String,
+      required: true,
+    },
+    contrastThread: {
+      type: String,
+      required: true,
+    },
+    insideThread: {
+      type: String,
+      required: true,
+    },
+    washCard: {
+      type: String,
+      required: true,
+    },
+    embroidery: {
+      type: String,
+      required: true,
+    },
+    zip: {
+      type: String,
       required: true,
     },
     // currentStage: {
