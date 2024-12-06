45ed1c4abceb49efc346377a76a446edbe2598ee
Pratik <pratikkhatri520@gmail.com>
2024-12-06 14:40:17 +0530

    added upload router

---
+++ src/routes/uploadRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { uploadFile } from "../controllers/uploadController.js";
+import upload from "../middleware/multerMiddleware.js";
+const router = Router();
+
+router.post("/upload", upload.single("file"), uploadFile);
+
+export default router;
